56139cbd528631124fbc93100c3b7af33d2ea81a
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-10-04 20:10:38 +0300

    commit tree 046870c33e1165a46c227bc60a9024f65808d7da

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,17 +4,24 @@ class ArticlesController < ApplicationController
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
+
+    render json: articles, user_serializer: ArticleListSerializer 
   end
 
   def show
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+
+    if session[:page_views] <= 3
     article = Article.find(params[:id])
     render json: article
+    else 
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
 
   private
-
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
-
 end
